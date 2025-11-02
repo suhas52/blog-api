5681509dfed1f12217347d9aadf05cc684389a04
suhas52 <suhasedu991@gmail.com>
2025-11-02 10:14:53 +0530

    set up a login and register endpoint with that creates a jsonwebtoken and uses res.cookie to set it to the browsers cookie

--- server/db/queries.js
+++ server/db/queries.js
@@ -9,11 +9,16 @@ async function getAllUsernames() {
 }
 
 async function loginUser(username) {
-    const { rows } = await pool.query("SELECT (id, username, is_admin, password) FROM users WHERE username = ($1)", [username]);
-    return rows;
+    const { rows } = await pool.query("SELECT id, username, is_admin, password FROM users WHERE username = ($1)", [username]);
+    return rows[0];
+}
+
+async function registerUser(name, username, hash) {
+    await pool.query("INSERT INTO users (name, username, password) VALUES ($1, $2, $3)", [name, username, hash])
 }
 
 module.exports = {
     getAllUsernames,
-    loginUser
+    loginUser,
+    registerUser
 }
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,14 +1,31 @@
 const express = require('express');
-const { loginUser } = require('../db/queries');
+const { loginUser, registerUser } = require('../db/queries');
 const router = express.Router();
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 router.post("/login", async (req, res) => {
-    const credentials = await loginUser(req.body.username, req.body.password);
-    const [server_id, server_username, server_is_admin, server_password] = credentials[0].row.slice(1, -1).split(',');
-    const match = await bcrypt.compare(req.body.password, server_password);
-    console.log(match)
+    const user = await loginUser(req.body.username, req.body.password)
+    const match = await bcrypt.compare(req.body.password, user.password);
+    if (match) {
+        const token = jwt.sign({ userId: user.id, username: user.username, isAdmin: user.is_admin }, process.env.JWT_SECRET, {
+            expiresIn: "1h",
+        });
+        res.cookie("token", token, {
+            httpOnly: true,
+            sameSite: "strict",
+            maxAge: 60 * 60 * 1000
+        });
+    }
 } 
 )
+
+router.post("/register", async (req, res) => {
+    const {name, username, password} = req.body;
+    const numSaltRounds = 8;
+    const hash = await bcrypt.hash(password, numSaltRounds);
+    registerUser(name, username, hash);
+})
 
 module.exports = router;
